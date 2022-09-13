f813a2b1061f32b47440522139ddb94ee0ed0e9b
Rylan Sanders <sandersrylan2@gmail.com>
2022-09-13 18:16:49 -0500

    Modify SetVar to work better

--- CustomOp/Operations/SetVarOperation.cs
+++ CustomOp/Operations/SetVarOperation.cs
@@ -10,42 +10,16 @@ namespace CustomOp.Operations
 {
     internal class SetVarOperation : Operation
     {
-        private OpData toSetData;
         public SetVarOperation(XElement config) : base(config)
         {
-            toSetData = new OpData();
-            toSetData = parseInput(config, toSetData);
         }
 
         public override void execute(OpData data)
         {
             base.execute(data);
 
-            data.merge(toSetData);
+            data.put(data.getString("OutputID"), data.getObject(data.getString("ObjectID")));
         }
 
-        public OpData parseInput(XElement element, OpData data)
-        {
-            var inputs = from input in element.Descendants("Data")
-                         select new
-                         {
-                             Name = input.Attribute("name").Value.ToString(),
-                             Value = input.Attribute("value").Value.ToString(),
-                             Type = input.Attribute("type").Value.ToString()
-                         };
-
-            foreach (var input in inputs)
-            {
-                if (input.Type.Equals("string"))
-                {
-                    data.put(input.Name, input.Value);
-                }
-                else if (input.Type.Equals("int"))
-                {
-                    data.put(input.Name, Int32.Parse(input.Value));
-                }
-            }
-            return data;
-        }
     }
 }
