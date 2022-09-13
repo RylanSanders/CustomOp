5063185d9066f1b2ce11aefe004b6428a4540a64
Rylan Sanders <sandersrylan2@gmail.com>
2022-09-13 18:16:18 -0500

    Make Process List Run Synchronously

--- CustomOp/Operations/ProcessListOperation.cs
+++ CustomOp/Operations/ProcessListOperation.cs
@@ -10,9 +10,21 @@ namespace CustomOp.Operations
     internal class ProcessListOperation : Operation
     {
         Process operatedProcess;
+        bool isSynchronous = false;
+        int waitTime = 0;
         public ProcessListOperation(XElement config) : base(config)
         {
             operatedProcess = new Process(config.Element("OperatedProcess").Element("Process"));
+            XElement synch = config.Element("Synchronous");
+            if(synch != null)
+            {
+                isSynchronous = Boolean.Parse(synch.Value);
+            }
+            XElement waitTimeElement = config.Element("WaitTime");
+            if (waitTimeElement != null)
+            {
+                waitTime = int.Parse(waitTimeElement.Value);
+            }
         }
 
         public override void execute(OpData data)
@@ -27,7 +39,11 @@ namespace CustomOp.Operations
                 tempData.put("ListIntVal", i);
                 Process spawnedThread = operatedProcess.clone();
                 spawnedThread.addOpData(tempData);
-                ExecutionController.runProcess(spawnedThread);
+                if (!isSynchronous)
+                    ExecutionController.runProcess(spawnedThread);
+                else
+                    ExecutionController.syncRunProcess(spawnedThread);
+                Thread.Sleep(waitTime);
             }
         }
     }
